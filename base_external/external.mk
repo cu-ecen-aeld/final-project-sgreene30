930d930b257fd08eda50f95183f8ad860e3a4351
sgreene30 <sgreene30@Ubuntu-Fresh.myguest.virtualbox.org>
2023-11-09 20:06:15 -0500

    added test aesd-assignments package

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,1 +1,2 @@
-include $(sort $(wildcard $(BR2_EXTERNAL_assignment_base_PATH)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_audio_base_PATH)/package/aesd-final-project/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_audio_base_PATH)/package/aesd-assignments/*.mk))
---
+++ base_external/package/aesd-final-project/aesd-final-project.mk
@@ -0,0 +1,28 @@
+
+##############################################################
+#
+# AESD Final Project
+#
+##############################################################
+
+AESD_ASSIGNMENTS_VERSION = 39634a90a36417cd55882d2cefc45562c2222534
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_ASSIGNMENTS_SITE = git@github.com:sgreene30/final-project-assignment-sgreene30.git
+AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = portAudioDemo
+AESD_ASSIGNMNETS_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
+define AUDIO_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/portAudioDemo 
+endef
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define AUDIO_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/portAudioDemo/instrument $(TARGET_DIR)/usr/bin
+
+endef
+
+$(eval $(generic-package))
