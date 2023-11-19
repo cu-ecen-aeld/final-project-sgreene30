cac525814a73194a85c426a0fec082e9bc57705b
sgreene30 <sgreene30@Ubuntu-Fresh.myguest.virtualbox.org>
2023-11-19 13:18:00 -0500

    added GPIO input support

--- base_external/package/audio/audio.mk
+++ base_external/package/audio/audio.mk
@@ -5,24 +5,26 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 39634a90a36417cd55882d2cefc45562c2222534
+AUDIO_VERSION = e52f432d12f54c49443f3f9803ed0da590845de9
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:sgreene30/final-project-assignment-sgreene30.git
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
-AESD_ASSIGNMENTS_MODULE_SUBDIRS = portAudioDemo
-AESD_ASSIGNMNETS_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+AUDIO_SITE = git@github.com:sgreene30/final-project-assignment-sgreene30.git
+AUDIO_SITE_METHOD = git
+AUDIO_GIT_SUBMODULES = YES
+AUDIO_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
 #define AUDIO_BUILD_CMDS
-#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/portAudioDemo 
+#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/portAudioDemo install-deps
+#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/portAudioDemo
+	
 #endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-#define AUDIO_INSTALL_TARGET_CMDS
+define AUDIO_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/gpio/input.py $(TARGET_DIR)/usr/bin
+endef
 #	$(INSTALL) -m 0755 $(@D)/portAudioDemo/instrument $(TARGET_DIR)/usr/bin
-
 #endef
 
 $(eval $(generic-package))
